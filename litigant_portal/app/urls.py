956bdb06252ed7fa8568d898a122452422a572b9
Nathan Dahlberg <nadahlberg@gmail.com>
2026-05-04 12:55:41 -0400

    feat(app): add base template

--- litigant_portal/app/urls.py
+++ litigant_portal/app/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="login"),
     path("chat/", views.chat, name="chat"),
     path("search/", views.search, name="search"),
     path("profile/", views.profile, name="profile"),
--- litigant_portal/app/views.py
+++ litigant_portal/app/views.py
@@ -5,6 +5,14 @@ def index(request):
     return render(request, "index.html")
 
 
+def register(request):
+    return render(request, "register.html")
+
+
+def login(request):
+    return render(request, "login.html")
+
+
 def chat(request):
     return render(request, "chat.html")
 
