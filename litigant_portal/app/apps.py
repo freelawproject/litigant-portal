4fd48ca1d4543eb536eaf5b8ee8aee86ba491e38
Nathan Dahlberg <nadahlberg@gmail.com>
2026-05-18 10:50:04 -0400

    feat(app): add blank app template to package

---
+++ litigant_portal/app/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig as DjangoAppConfig
+
+
+class AppConfig(DjangoAppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "litigant_portal.app"
+
+    def ready(self):
+        import litigant_portal.app.checks  # noqa: F401
+        import litigant_portal.app.signals  # noqa: F401
