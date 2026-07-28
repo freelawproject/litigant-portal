3439fbca1083f838418633107e2fa28b6cc58951
Nathan Dahlberg <nadahlberg@gmail.com>
2026-07-28 09:25:29 -0400

    feat(app): update imports for assistant and model init

--- litigant_portal/app/models/__init__.py
+++ litigant_portal/app/models/__init__.py
@@ -1,4 +1,5 @@
-from .admin import *  # noqa: F403
-from .assistant import *  # noqa: F403
 from .chat_engine import *  # noqa: F403
+from .site import *  # noqa: F403
+from .topic_flow import *  # noqa: F403
+from .upload import *  # noqa: F403
 from .user import *  # noqa: F403
