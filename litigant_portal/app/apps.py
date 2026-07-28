4e243b3ed5ccfb11606ea4bbdbcfeccdc273c314
Nathan Dahlberg <nadahlberg@gmail.com>
2026-07-28 11:02:04 -0400

    feat(app): delete old topic flow

--- litigant_portal/app/apps.py
+++ litigant_portal/app/apps.py
@@ -8,4 +8,3 @@ class AppConfig(DjangoAppConfig):
     def ready(self):
         import litigant_portal.app.checks  # noqa: F401
         import litigant_portal.app.signals  # noqa: F401
-        import litigant_portal.app.topic_flow.checks  # noqa: F401
